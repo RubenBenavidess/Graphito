14a13097bd184589e828e23428397dac68855d04
Ruben Dario Benavides Macias <dario.benavides@hotmail.com>
2024-12-11 12:43:24 -0500

    Requisitos de Gestión de Archivos 1, 2 y 3

--- FileManager.cs
+++ FileManager.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,75 @@ namespace Graphito
 {
     internal static class FileManager
     {
-        //private String LastFilePath;
+        private static String LastFilePath;
+        public static bool Save(Canvas canvas, string extension, string path, string fileName)
+        {
+
+            if (String.IsNullOrWhiteSpace(extension) || String.IsNullOrWhiteSpace(path) || String.IsNullOrWhiteSpace(fileName))
+            {
+                Console.WriteLine("Error al guardar el archivo: Campos Incorrectos");
+                return false;
+            }
+
+            ImageFormat format;
+            extension = extension.ToLowerInvariant();
+
+            switch (extension)
+            {
+                case "jpeg":
+                    format = ImageFormat.Jpeg;
+                    break;
+                case "png":
+                    format = ImageFormat.Png;
+                    break;
+                case "bmp":
+                    format = ImageFormat.Bmp;
+                    break;
+                default:
+                    Console.WriteLine("Error al guardar el archivo: Extensión Inválida");
+                    return false;
+            }
+
+            try
+            {
+                string fullPath = Path.Combine(path, fileName);
+                canvas.bmp.Save(fullPath, format);
+                Console.WriteLine("Archivo guardado exitosamente.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al guardar el archivo: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool Load(Canvas canvas, String filePath)
+        {
+            
+            string extension = Path.GetExtension(filePath)?.ToLowerInvariant();
+            if (string.IsNullOrEmpty(extension) || (extension != ".jpeg" && extension != ".png" && extension != ".bmp"))
+            {
+                Console.WriteLine("Error: Extensión de archivo no soportada.");
+                return false;
+            }
+
+            try
+            {
+                Image image = Image.FromFile(filePath);
+                canvas.LoadBitmap(new Bitmap(image));
+                canvas.Refresh();
+
+                Console.WriteLine("Archivo cargado exitosamente.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cargar el archivo: " + ex.Message);
+                return false;
+            }
+
+        }
+
     }
 }
