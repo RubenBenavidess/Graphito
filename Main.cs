481c2c4281e6fe6d3aa0a6f0adc56839f63869f4
Edison Damian Verdesoto Segovia <132785383+EDVerdesoto@users.noreply.github.com>
2024-12-12 11:45:21 -0500

    comentarios

--- Main.cs
+++ Main.cs
@@ -34,6 +34,7 @@ namespace Graphito
             InitializeData();
         }
 
+        //boton para guardar la imagen
         private void itmSave_Click(object sender, EventArgs e)
         {
             try
@@ -45,7 +46,7 @@ namespace Graphito
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
-
+        //metodo para cargar el canvas
         private void itmLoad_Click(object sender, EventArgs e)
         {
             try
@@ -57,39 +58,43 @@ namespace Graphito
             }
             
         }
-
+        //reajustar la pizarra
         private void Main_Resize(object sender, EventArgs e)
         {
             this.picCanvas.Left = (this.ClientSize.Width - this.picCanvas.Width) / 2;
         }
-
+            
+        //Limpiar la pizarra
         private void itmNew_Click(object sender, EventArgs e)
         {
             picCanvas.Renew();
         }
-
+        
+        //funcion del click de UnDo
         private void strItemUndo_Click(object sender, EventArgs e)
         {
             picCanvas.LoadBitmap(ActionsRecordManager.Undo((Bitmap)picCanvas.bmp.Clone()));
         }
 
+        //Funcion del click de ReDo
         private void strItemRedo_Click(object sender, EventArgs e)
         {
             picCanvas.LoadBitmap(ActionsRecordManager.Redo((Bitmap)picCanvas.bmp.Clone()));
         }
 
+        //Funcion para capturar el ctrl + z y el ctrl + y
         private void Main_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Control && e.KeyCode == Keys.Z)
+            if(e.Control && e.KeyCode == Keys.Z) //ctrl + z
             {
                 picCanvas.LoadBitmap(ActionsRecordManager.Undo((Bitmap)picCanvas.bmp.Clone()));
             }
-            if (e.Control && e.KeyCode == Keys.Y)
+            if (e.Control && e.KeyCode == Keys.Y) // ctrl + y
             {
                 picCanvas.LoadBitmap(ActionsRecordManager.Redo((Bitmap)picCanvas.bmp.Clone()));
             }
         }
-
+        //levantar el mouse y salir del foco hacia el main
         private void picCanvas_MouseUp(object sender, MouseEventArgs e)
         {
             this.ActiveControl = null;
